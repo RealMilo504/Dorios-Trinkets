293b932df8488eeac90806bf17ab4666511dccb4
Dorios Studios <chavez100577@gmail.com>
2026-06-12 13:31:25 -0600

    Added inanimate family

--- BP/scripts/main.js
+++ BP/scripts/main.js
@@ -1,6 +1,4 @@
-import './block_tick.js'
-import './register.js'
-import './system.js'
-import './Core/main.js'
-import './insight_registry_injector.generated.js'
-
+import "./block_tick.js";
+import "./register.js";
+import "./system.js";
+import "./Core/main.js";
